9cf001ad257b6e2b8f3ea76c219037289c6dfda1
Ashot1995 <ashotgharakeshishyan@gmail.com>
2020-11-02 00:23:21 +0400

    fix

--- index.php
+++ index.php
@@ -12,8 +12,6 @@
             crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-
 </head>
 <body>
 <div class="d-flex">
